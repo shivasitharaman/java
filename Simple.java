e5766b659d60b760f3620f0cf2d5f6ecf2bd529e
shivasitharaman <40603401+shivasitharaman@users.noreply.github.com>
2018-09-24 15:22:55 +0530

    Add files via upload

---
+++ Simple.java
@@ -0,0 +1,11 @@
+import java.util.StringTokenizer;  
+public class Simple {
+
+	public static void main(String args[]){  
+		   StringTokenizer st = new StringTokenizer("my name is Shiva"," ");  
+		     while (st.hasMoreTokens()) {  
+		         System.out.println(st.nextToken());  
+		     }  
+		   }
+
+}
