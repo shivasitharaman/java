d060908734374a547c25ddbb527d7cddd03289b2
shivasitharaman <40603401+shivasitharaman@users.noreply.github.com>
2018-09-24 14:40:38 +0530

    Add files via upload

--- Multi3.java
+++ Multi3.java
@@ -1,4 +1,5 @@
+package java;
 
-public class Multi3 {
+public interface Multi3 {
 
 }
---
+++ Person.java
@@ -0,0 +1,15 @@
+package java;
+
+public abstract class Person {
+
+	abstract void eat();  
+	}  
+	class TestAnonymousInner{  
+	 public static void main(String args[]){  
+	  Person p=new Person(){  
+	  void eat(){System.out.println("nice fruits");}  
+	  };  
+	  p.eat();  
+	 }
+
+}
